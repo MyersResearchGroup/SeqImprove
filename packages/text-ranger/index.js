46d109c74dfeca2834323b11f687c2e9e7b055a0
Zach Sents <zachsents@gmail.com>
2022-11-18 18:48:11 -0700

    Created new text range package

---
+++ packages/text-ranger/index.js
@@ -0,0 +1,72 @@
+
+
+export class TextBuffer {
+
+    constructor(text) {
+        this.text = text
+        this.originalText = text
+
+        this.transforms = []
+    }
+
+    modifyRange(start, end, newText) {
+        /**
+         * Start by projecting the start and end indeces against the existing
+         * transforms.
+         */
+        start = this.projectIndex(start)
+        end = this.projectIndex(end)
+
+        // Enforce ordering conventions
+        if (start > end)
+            throw Error("Start index must be less than or equal to end index.")
+
+        // Calculate original text length
+        const originalLength = end - start
+
+        /**
+         * Any replacements that result in less characters than the original text
+         * will result in a range where the result has been removed.
+         */
+        if (newText.length < originalLength)
+            this.transforms.push(createErrorRange(start + newText.length, end))
+
+        /**
+         * Everything after the end index needs to be adjusted by the difference
+         * between the new length and the original length.
+         */
+        const adjustment = newText.length - originalLength
+        this.transforms.push(createTransform(end, adjustment))
+
+        // Make the replacement
+        this.text = this.text.slice(0, start) + newText + this.text.slice(end)
+    }
+
+    projectIndex(index) {
+        return this.transforms.reduce((accum, transform) => transform(accum), index)
+    }
+
+    getText(start, end) {
+        // Enforce ordering conventions
+        if (start > end)
+            throw Error("Start index must be less than or equal to end index.")
+
+        return start === undefined ? this.text : this.text.slice(
+            this.projectIndex(start),
+            this.projectIndex(end ?? start + 1)
+        )
+    }
+}
+
+
+function createTransform(threshold, adjustment) {
+    return index => index + (index >= threshold ? adjustment : 0)
+}
+
+function createErrorRange(start, end) {
+    return index => {
+        if (index >= start && index < end)
+            throw new Error(`Index ${index} has been removed.`)
+        return index
+    }
+}
---
+++ packages/text-ranger/test.js
@@ -0,0 +1,22 @@
+import { TextBuffer } from "./index.js"
+
+
+const buf = new TextBuffer("This part produces GFP. It was tested in E. coli  and is thought to work in B. subtilis. The circuit functions better when background levels of lactose are low. There is also lactse. E. coli  is the same as Escherichia coli.  Escichia coi.")
+
+console.log(buf.getText())
+console.log()
+
+buf.modifyRange(5, 9, "cool part")
+
+console.log(buf.getText())
+console.log()
+
+buf.modifyRange(19, 22, "[GFP](code)")
+
+console.log(buf.getText())
+console.log()
+
+buf.modifyRange(41, 48, "[E. coli](...SO562...)")
+
+console.log(buf.getText())
+console.log()
