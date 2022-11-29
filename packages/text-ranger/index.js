317eed5f37f9ea0249c038c66f44a25f474fe9d9
Zach Sents <zachsents@gmail.com>
2022-11-28 17:43:58 -0700

    Improved text-ranger

--- packages/text-ranger/index.js
+++ packages/text-ranger/index.js
@@ -1,69 +1,231 @@
-
+import { diffWordsWithSpace } from "diff"
 
 export class TextBuffer {
 
+    /**
+     * @type {string}
+     * @memberof TextBuffer
+     */
+    originalText
+
+    /**
+     * @type {Alias[]}
+     * @memberof TextBuffer
+     */
+    aliases
+
+    /**
+     * Creates an instance of TextBuffer.
+     * @param {string} text
+     * @memberof TextBuffer
+     */
     constructor(text) {
-        this.text = text
         this.originalText = text
 
-        this.transforms = []
+        this.aliases = []
     }
 
-    modifyRange(start, end, newText) {
-        /**
-         * Start by projecting the start and end indeces against the existing
-         * transforms.
-         */
-        start = this.projectIndex(start)
-        end = this.projectIndex(end)
-
-        // Enforce ordering conventions
-        if (start > end)
-            throw Error("Start index must be less than or equal to end index.")
-
-        // Calculate original text length
-        const originalLength = end - start
-
-        // If new text is a function, pass the original text to it
-        const originalText = this.text.slice(start, end)
-        if(typeof newText === "function")
-            newText = newText(originalText)
-
-        /**
-         * Any replacements that result in less characters than the original text
-         * will result in a range where the result has been removed.
-         */
-        if (newText.length < originalLength)
-            this.transforms.push(createErrorRange(start + newText.length, end))
-
-        /**
-         * Everything after the end index needs to be adjusted by the difference
-         * between the new length and the original length.
-         */
-        const adjustment = newText.length - originalLength
-        this.transforms.push(createTransform(end, adjustment))
-
-        // Make the replacement
-        this.text = this.text.slice(0, start) + newText + this.text.slice(end)
-        return originalText
+    /**
+     * Registers an Alias to this TextBuffer.
+     *
+     * @param {Alias} alias
+     * @memberof TextBuffer
+     */
+    register(alias) {
+        if (!this.isAliasRegistered(alias))
+            this.aliases.push(alias)
+        alias.registerTo(this)
     }
 
-    projectIndex(index) {
-        return this.transforms.reduce((accum, transform) => transform(accum), index)
+    /**
+     * Checks if an Alias is registered.
+     *
+     * @param {Alias} alias
+     * @return {boolean} 
+     * @memberof TextBuffer
+     */
+    isAliasRegistered(alias) {
+        return this.aliases.includes(alias)
     }
 
-    getText(start, end) {
-        // Enforce ordering conventions
-        if (start > end)
-            throw Error("Start index must be less than or equal to end index.")
+    /**
+     * Creates and registers an Alias.
+     *
+     * @param {number} start
+     * @param {number} end
+     * @param {string | function} text
+     * @return {Alias} 
+     * @memberof TextBuffer
+     */
+    createAlias(start, end, text) {
+        const alias = new Alias(start, end, text)
+        this.register(alias)
+        return alias
+    }
 
-        return start === undefined ? this.text : this.text.slice(
-            this.projectIndex(start),
-            end == null ? this.text.length : this.projectIndex(end)
-        )
+    /**
+     * Creates and returns the text with all enabled aliases in effect.
+     *
+     * @return {*} 
+     * @memberof TextBuffer
+     */
+    getText() {
+        const transforms = []
+
+        return this.aliases
+            .filter(a => a.enabled)
+            .reduce(
+                (accum, currentAlias) => {
+
+                    // Project start and end indices
+                    const start = projectIndex(currentAlias.start, transforms)
+                    const end = projectIndex(currentAlias.end, transforms)
+
+                    // Grab original text slice
+                    const originalText = accum.slice(start, end)
+
+                    // If new text is a function, pass the original text to it
+                    const newText = typeof currentAlias.text === "function" ?
+                        currentAlias.text(originalText) :
+                        currentAlias.text
+
+                    // Any replacements that result in less characters than the original text
+                    //  will result in a range where the result has been removed.
+                    if (newText.length < originalText.length)
+                        transforms.push(createErrorRange(start + newText.length, end))
+
+                    // Everything after the end index needs to be adjusted by the difference
+                    //  between the new length and the original length.
+                    const adjustment = newText.length - originalText.length
+                    transforms.push(createTransform(end, adjustment))
+
+                    // Make the replacement and return
+                    return accum.slice(0, start) + newText + accum.slice(end)
+                },
+
+                // Start reduction with original text
+                this.originalText
+            )
+    }
+
+    changeText(newText) {
+
+        // diff the old text with the new text
+        const diffs = diffWordsWithSpace(this.originalText, newText)
+
+        // loop through diffs
+        diffs.forEach((diff, i) => {
+            // calculate start & end
+            const start = diffs.slice(0, i).reduce(
+                (accum, current) => current.removed ? accum : accum + current.value.length,
+                0
+            )
+            const end = start + diff.value.length
+
+            // if there was an addition...
+            if (diff.added) {
+                // adjust aliases starting after the insertion point
+                this.aliases.forEach(alias => {
+                    if (alias.start > start) {
+                        alias.start += diff.value.length
+                        alias.end += diff.value.length
+                    }
+                    else if (alias.start == start)
+                        alias.end = end
+                })
+            }
+
+            // if there was a removal...
+            if (diff.removed) {
+                // adjust aliases starting after the removal
+                this.aliases.forEach(alias => {
+                    if (alias.start >= end) {
+                        alias.start -= diff.value.length
+                        alias.end -= diff.value.length
+                    }
+                    else if (alias.start >= start) {
+                        alias.start = start
+                        alias.end = start
+                    }
+                })
+            }
+        })
+
+        this.originalText = newText
     }
 }
 
+class Alias {
+
+    /**
+     * @type {number}
+     * @memberof Alias
+     */
+    start
+
+    /**
+     * @type {number}
+     * @memberof Alias
+     */
+    end
+
+    /**
+     * @type {string | function}
+     * @memberof Alias
+     */
+    text
+
+    /**
+     * @type {boolean}
+     * @memberof Alias
+     */
+    enabled
+
+    /**
+     * Creates an instance of Alias.
+     * @param {number} start
+     * @param {number} end
+     * @param {string | function} text
+     * @memberof Alias
+     */
+    constructor(start, end, text) {
+
+        if (start > end)
+            throw Error("Start index must be less than or equal to end index.")
+
+        this.start = start
+        this.end = end
+        this.text = text
+
+        this.enabled = false
+    }
+
+    /**
+     * Registers an alias to a TextBuffer.
+     *
+     * @param {TextBuffer} buffer
+     * @memberof Alias
+     */
+    registerTo(buffer) {
+        this.buffer = buffer
+        if (!buffer.isAliasRegistered(this))
+            buffer.register(this)
+    }
+
+    enable() {
+        this.enabled = true
+    }
+
+    disable() {
+        this.enabled = false
+    }
+}
+
+
+
+function projectIndex(index, transforms) {
+    return transforms.reduce((accum, transform) => transform(accum), index)
+}
 
 function createTransform(threshold, adjustment) {
     return index => index + (index >= threshold ? adjustment : 0)
--- packages/text-ranger/test.js
+++ packages/text-ranger/test.js
@@ -3,21 +3,19 @@ import { TextBuffer } from "./index.js"
 
 const buf = new TextBuffer("This part produces GFP. It was tested in E. coli  and is thought to work in B. subtilis. The circuit functions better when background levels of lactose are low. There is also lactse. E. coli  is the same as Escherichia coli.  Escichia coi.")
 
-console.log(buf.getText())
-console.log()
+const alias1 = buf.createAlias(5, 9, "cool part")
+const alias2 = buf.createAlias(19, 22, old => `[${old}](code)`)
+const alias3 = buf.createAlias(93, 100, "[circuit](...SO562...)")
 
-buf.modifyRange(5, 9, "cool part")
+console.log("Original\n----\n", buf.getText(), "\n")
 
-console.log(buf.getText())
-console.log()
+alias1.enable()
+alias2.enable()
+alias3.enable()
 
-// buf.modifyRange(19, 22, "[GFP](code)")
-buf.modifyRange(19, 22, old => `[${old}](code)`)
+console.log("After Alias 1, 2, & 3\n----\n", buf.getText(), "\n")
 
-console.log(buf.getText())
-console.log()
+buf.changeText("This part makes PR. There is a new sentence. It was tested in E. coli and works in B. subtilis. The circuit functions better when background levels of lactose are low. There is also lactse. E. coli  is the same as Escherichia coli.  Escichia coi.")
 
-buf.modifyRange(41, 48, "[E. coli](...SO562...)")
 
-console.log(buf.getText())
-console.log()
+console.log("After text change\n----\n", buf.getText(), "\n")
