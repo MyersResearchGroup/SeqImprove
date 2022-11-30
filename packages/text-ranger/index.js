902fb887b7239300ad83931ba6b7bf392d316cf4
Zach Sents <zachsents@gmail.com>
2022-11-30 00:18:21 -0700

    Added ability to get projected indeces from text-ranger

--- packages/text-ranger/index.js
+++ packages/text-ranger/index.js
@@ -69,43 +69,52 @@ export class TextBuffer {
      * @return {*} 
      * @memberof TextBuffer
      */
-    getText() {
+    getText(includeData = false) {
         const transforms = []
+        const enabledAliases = this.aliases.filter(a => a.enabled)
 
-        return this.aliases
-            .filter(a => a.enabled)
-            .reduce(
-                (accum, currentAlias) => {
+        const text = enabledAliases.reduce(
+            (accum, currentAlias) => {
 
-                    // Project start and end indices
-                    const start = projectIndex(currentAlias.start, transforms)
-                    const end = projectIndex(currentAlias.end, transforms)
+                // Project start and end indices
+                const start = projectIndex(currentAlias.start, transforms)
+                const end = projectIndex(currentAlias.end, transforms)
 
-                    // Grab original text slice
-                    const originalText = accum.slice(start, end)
+                // Grab original text slice
+                const originalText = accum.slice(start, end)
 
-                    // If new text is a function, pass the original text to it
-                    const newText = typeof currentAlias.text === "function" ?
-                        currentAlias.text(originalText) :
-                        currentAlias.text
+                // If new text is a function, pass the original text to it
+                const newText = typeof currentAlias.text === "function" ?
+                    currentAlias.text(originalText) :
+                    currentAlias.text
 
-                    // Any replacements that result in less characters than the original text
-                    //  will result in a range where the result has been removed.
-                    if (newText.length < originalText.length)
-                        transforms.push(createErrorRange(start + newText.length, end))
+                // Any replacements that result in less characters than the original text
+                //  will result in a range where the result has been removed.
+                if (newText.length < originalText.length)
+                    transforms.push(createErrorRange(start + newText.length, end))
 
-                    // Everything after the end index needs to be adjusted by the difference
-                    //  between the new length and the original length.
-                    const adjustment = newText.length - originalText.length
-                    transforms.push(createTransform(end, adjustment))
+                // Everything after the end index needs to be adjusted by the difference
+                //  between the new length and the original length.
+                const adjustment = newText.length - originalText.length
+                transforms.push(createTransform(end, adjustment))
 
-                    // Make the replacement and return
-                    return accum.slice(0, start) + newText + accum.slice(end)
-                },
+                // Make the replacement and return
+                return accum.slice(0, start) + newText + accum.slice(end)
+            },
 
-                // Start reduction with original text
-                this.originalText
-            )
+            // Start reduction with original text
+            this.originalText
+        )
+
+        // Optionally include projected indeces as well
+        return includeData ? {
+            text,
+            patches: enabledAliases.map(alias => ({
+                alias,
+                projectedStart: projectIndex(alias.start, transforms),
+                projectedEnd: projectIndex(alias.end, transforms),
+            }))
+        } : text
     }
 
     changeText(newText) {
