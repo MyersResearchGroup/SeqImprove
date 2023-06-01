2386e3d337d564800b7042718fb7e0f28f8aece9
Zach Sents <zachsents@gmail.com>
2023-05-31 18:09:55 -0600

    Filtered out sequence annotations that don't have both start and end references to the split text.
    Looks like this problem was caused by duplicate and potentially overlapping sequence annotations.

--- apps/web/src/components/SequenceHighlighter.jsx
+++ apps/web/src/components/SequenceHighlighter.jsx
@@ -58,65 +58,70 @@ export default function SequenceHighlighter({ sequence, annotations, onChange, w
                 {...scrollAreaProps}
                 styles={{ viewport: { position: "relative", paddingTop: 4 } }}
             >
-                {annotations.map(anno => {
-                    const span = annotationSpans[anno.id]
-                    const ref = refs.current[anno.id]
+                {annotations
+                    .filter(anno => {
+                        const ref = refs.current[anno.id]
+                        return ref?.start && ref?.end
+                    })
+                    .map(anno => {
+                        const span = annotationSpans[anno.id]
+                        const ref = refs.current[anno.id]
 
-                    return (
-                        <Box
-                            pos="absolute"
-                            left={0}
-                            top={refs.current[anno.id]?.start.offsetTop - 4}
-                            w="100%"
-                            // maw={maxContainerWidth + 30}
-                            key={anno.id}
-                        >
+                        return (
                             <Box
-                                display="inline"
-                                ml={`calc(${ref?.start.offsetLeft - 2 + spacing / 2}px + ${span?.start.char - 1}ch)`}
-                                bg={anno.active && `${anno.color}.2`}
-                                onClick={() => onChange?.(anno.id, !anno.active)}
-                                sx={theme => ({
-                                    borderRadius: 5,
-                                    cursor: "pointer",
-                                    // border: anno.active ? "none" : `3px solid ${theme.colors[anno.color][3]}`,
-                                    border: `3px solid ${theme.colors[anno.color][anno.active ? 5 : 2]}`,
-                                })}
+                                pos="absolute"
+                                left={0}
+                                top={ref?.start.offsetTop - 4}
+                                w="100%"
+                                // maw={maxContainerWidth + 30}
+                                key={anno.id}
                             >
-                                <Text
-                                    {...textProps}
-                                    px={undefined}
-                                    pr={spacing / 2}
-                                    // color={anno.color}
-                                    c="transparent"
+                                <Box
+                                    display="inline"
+                                    ml={`calc(${ref?.start.offsetLeft - 2 + spacing / 2}px + ${span?.start.char - 1}ch)`}
+                                    bg={anno.active && `${anno.color}.2`}
+                                    onClick={() => onChange?.(anno.id, !anno.active)}
+                                    sx={theme => ({
+                                        borderRadius: 5,
+                                        cursor: "pointer",
+                                        // border: anno.active ? "none" : `3px solid ${theme.colors[anno.color][3]}`,
+                                        border: `3px solid ${theme.colors[anno.color][anno.active ? 5 : 2]}`,
+                                    })}
                                 >
-                                    {Array(wordSize - (span?.start.char - 1)).fill("_").join("")}
-                                </Text>
-
-                                {Array(span?.end.word - span?.start.word - 1).fill(0).map((_, i) =>
                                     <Text
                                         {...textProps}
+                                        px={undefined}
+                                        pr={spacing / 2}
                                         // color={anno.color}
                                         c="transparent"
-                                        key={"filler" + i}
                                     >
-                                        {Array(wordSize).fill("_").join("")}
+                                        {Array(wordSize - (span?.start.char - 1)).fill("_").join("")}
                                     </Text>
-                                )}
 
-                                <Text
-                                    {...textProps}
-                                    px={undefined}
-                                    pl={spacing / 2}
-                                    // color={anno.color}
-                                    c="transparent"
-                                >
-                                    {Array(span?.end.char).fill("_").join("")}
-                                </Text>
+                                    {Array(span?.end.word - span?.start.word - 1).fill(0).map((_, i) =>
+                                        <Text
+                                            {...textProps}
+                                            // color={anno.color}
+                                            c="transparent"
+                                            key={"filler" + i}
+                                        >
+                                            {Array(wordSize).fill("_").join("")}
+                                        </Text>
+                                    )}
+
+                                    <Text
+                                        {...textProps}
+                                        px={undefined}
+                                        pl={spacing / 2}
+                                        // color={anno.color}
+                                        c="transparent"
+                                    >
+                                        {Array(span?.end.char).fill("_").join("")}
+                                    </Text>
+                                </Box>
                             </Box>
-                        </Box>
-                    )
-                })}
+                        )
+                    })}
 
                 {splitSequence.map((word, i) =>
                     <Text
@@ -163,10 +168,8 @@ function createRefForWord(spans, refs, wordIndex) {
 
     // create ref function to insert element in refs object
     return el => {
-        if (refs.current[id])
-            refs.current[id][position] = el
-        else
-            refs.current[id] = { [position]: el }
+        refs.current[id] ??= {}
+        refs.current[id][position] = el
     }
 }
 
